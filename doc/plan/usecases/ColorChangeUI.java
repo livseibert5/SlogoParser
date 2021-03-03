5390e5943830180df55c48484ea829edc7e64d9c
Jessica Yang <jessica.yang@duke.edu>
2021-03-02 21:52:25 -0500

    Added usecase and wireframe.

---
+++ doc/plan/usecases/ColorChangeUI.java
@@ -0,0 +1,5 @@
+/**
+ * The user sets the pen's color using the UI so subsequent lines drawn when the turtle moves use that color.
+ */
+// On dropdown selection, the following method is called with the appropriate color codes.
+ FrontEndExternal.changePenColor(int r, int g, int b);
