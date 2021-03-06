feed15bd7c8606012d94d368a5d0da0be8f8f44a
Jessica Yang <jessica.yang@duke.edu>
2021-03-06 17:13:57 -0500

    Added WindowControl.

---
+++ src/slogo/view/WindowControl.java
@@ -0,0 +1,54 @@
+package slogo.view;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import slogo.controller.Controller;
+import slogo.model.Turtle;
+
+/**
+ * Creates stage, scene, and animation.
+ * TODO assumptions, dependencies, example of use
+ *
+ * @author Jessica Yang
+ */
+public class WindowControl {
+
+  public static final String TITLE = "SLogo";
+
+  private Stage myStage;
+  //private CreateScene myScene;
+
+  private static final int FRAMES_PER_SECOND = 2;
+  private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+  private static final Timeline animation = new Timeline();
+
+  private Controller myController = new Controller();
+  private static TurtleDisplay myTurtleDisplay;
+
+  /**
+   * Constructor for WindowControl class. Returns WindowControl object.
+   */
+  public WindowControl() {
+    myStage = new Stage();
+    myStage.setResizable(false);
+
+    //myStage.setScene(myScene.getScene());
+    myStage.setTitle(TITLE);
+    myStage.show();
+
+    myTurtleDisplay = new TurtleDisplay(myController.getTurtleMap());
+  }
+
+  /**
+   * Starts animation for Turtle.
+   */
+  // on observer change, update! TODO
+  private static void animationStart() {
+    KeyFrame frame = new KeyFrame(Duration.seconds(SECOND_DELAY), event -> myTurtleDisplay.step());
+    animation.setCycleCount(Timeline.INDEFINITE);
+    animation.getKeyFrames().add(frame);
+    animation.play();
+  }
+}
