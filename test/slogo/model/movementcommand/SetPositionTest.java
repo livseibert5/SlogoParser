a02c63862a54889a855beea4aeb90e619cae4123
Livia <las120@duke.edu>
2021-03-06 17:53:48 -0500

    wrote SetPosition tests

---
+++ test/slogo/model/movementcommand/SetPositionTest.java
@@ -0,0 +1,33 @@
+package slogo.model.movementcommand;
+
+import org.junit.jupiter.api.Test;
+import slogo.model.Constant;
+import slogo.model.Turtle;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for SetPosition class.
+ *
+ * @author Livia Seibert
+ */
+public class SetPositionTest {
+
+  private Turtle turtle = new Turtle();
+  private SetPosition setPosition;
+
+  @Test
+  void moveToCurrentLocation() {
+    setPosition = new SetPosition(new Constant(0), new Constant(0));
+    setPosition.execute(turtle);
+    assertEquals(0, turtle.getXCoordinate());
+    assertEquals(0, turtle.getYCoordinate());
+  }
+
+  @Test
+  void moveToNewLocation() {
+    setPosition = new SetPosition(new Constant(10), new Constant(15));
+    setPosition.execute(turtle);
+    assertEquals(10, turtle.getXCoordinate());
+    assertEquals(15, turtle.getYCoordinate());
+  }
+}
