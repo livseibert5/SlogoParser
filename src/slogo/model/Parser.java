0e102afa6586c932284eb108bf527a8e34cb0fdc
Livia <las120@duke.edu>
2021-03-07 17:37:53 -0500

    refactored Parser into more helper methods

--- src/slogo/model/Parser.java
+++ src/slogo/model/Parser.java
@@ -13,9 +13,7 @@ public class Parser {
   private RegexDetector regexDetector = new RegexDetector();
   private CommandFactory commandFactory = new CommandFactory();
   private Controller controller;
-  //private Stack<String> commandStack;
   private Stack<Object> commandStack;
-  //private Stack<Constant> argumentStack;
   private Stack<Object> argumentStack;
   private static final String RESOURCES_PACKAGE = "resources.languages.";
   private Turtle turtle;
@@ -23,14 +21,15 @@ public class Parser {
   public Parser(Controller controller) {
     this.controller = controller;
     turtle = controller.getTurtleHandler().getTurtle();
-    commandStack = new Stack<>();
-    argumentStack = new Stack<>();
-    regexDetector.addPatterns("English");
-    regexDetector.addPatterns("Syntax");
+    setUpParser();
   }
 
   public Parser(Turtle turtle) {
     this.turtle = turtle;
+    setUpParser();
+  }
+
+  private void setUpParser() {
     commandStack = new Stack<>();
     argumentStack = new Stack<>();
     regexDetector.addPatterns("English");
@@ -39,52 +38,53 @@ public class Parser {
 
   public int parse(String command)
       throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-    System.out.println(command);
     String[] commandComponents = command.split(" ");
-    /*
-    for (String commandComponent: commandComponents) {
-      String commandType = regexDetector.getSymbol(commandComponent);
-      if (commandType.equals("Constant")) {
-        commandStack.push(commandComponent);
-      } else {
-        commandStack.push(commandType);
-      }
-      System.out.println(commandType);
-    }*/
     int i = 0;
     while (i < commandComponents.length) {
       String commandType = regexDetector.getSymbol(commandComponents[i]);
       if (commandType.equals("ListStart")) {
-        int openParenIndex = i;
-        int parenCount = 1;
-        while (parenCount != 0) {
-          i++;
-          commandType = regexDetector.getSymbol(commandComponents[i]);
-          if (commandType.equals("ListStart")) {
-            parenCount++;
-          } else if (commandType.equals("ListEnd")) {
-            parenCount--;
-          }
-        }
-        List<String> commandBlock = Arrays.asList(Arrays.copyOfRange(commandComponents, openParenIndex + 1, i));
-        StringBuilder block = new StringBuilder();
-        for (String commandPiece: commandBlock) {
-          block.append(commandPiece);
-          block.append(" ");
-        }
-        block.deleteCharAt(block.length() - 1);
-        CommandBlock codeBlock = new CommandBlock(block.toString());
-        commandStack.push(codeBlock);
+        int endParenIndex = findEndOfOuterParenthesesBlock(i, commandComponents);
+        List<String> commandBlock = Arrays.asList(Arrays.copyOfRange(commandComponents, i + 1, findEndOfOuterParenthesesBlock(i, commandComponents)));
+        i = endParenIndex;
+        commandStack.push(listToCommandBlock(commandBlock));
       } else if (commandType.equals("Constant")) {
         commandStack.push(new Constant(Integer.parseInt(commandComponents[i])));
       } else {
         commandStack.push(commandType);
       }
-      System.out.println(commandStack.peek());
+      if (commandStack.peek() instanceof Constant) {
+        System.out.println(((Constant) commandStack.peek()).getValue());
+      } else {
+        System.out.println(commandStack.peek());
+      }
       i++;
     }
     parseCommandStack();
     return 0;
+  }
+
+  private int findEndOfOuterParenthesesBlock(int index, String[] commandComponents) {
+    int parenCount = 1;
+    while (parenCount != 0) {
+      index++;
+      String commandType = regexDetector.getSymbol(commandComponents[index]);
+      if (commandType.equals("ListStart")) {
+        parenCount++;
+      } else if (commandType.equals("ListEnd")) {
+        parenCount--;
+      }
+    }
+    return index;
+  }
+
+  private CommandBlock listToCommandBlock(List<String> commandBlock) {
+    StringBuilder block = new StringBuilder();
+    for (String commandPiece: commandBlock) {
+      block.append(commandPiece);
+      block.append(" ");
+    }
+    block.deleteCharAt(block.length() - 1);
+    return new CommandBlock(block.toString());
   }
 
   private void parseCommandStack()
@@ -103,23 +103,17 @@ public class Parser {
           }
         }
         Command newCommand = (Command) commandFactory.createCommand((String) command, parameters);
-        argumentStack.push(new Constant(
-            (int) newCommand.execute(turtle)));
-        // Object newCommand = commandFactory.createCommand(commandStack.pop(), )
-        // pop command off command stack and determine how many arguments it takes
-        // if the argument stack is greater than or equal to the number of arguments needed, create
-        // a command object and pass it the two commands.
-        // execute this new command object and push it on to the argument stack.
+        argumentStack.push(new Constant((int) newCommand.execute(turtle)));
       }
     }
     if (!argumentStack.isEmpty()) {
-      System.out.println(((Constant) argumentStack.pop()).getValue());
+      //System.out.println(((Constant) argumentStack.pop()).getValue());
     }
   }
 
   public static void main(String[] args)
       throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
     Parser parser = new Parser(new Controller());
-    parser.parse("fd sum 10 sum 10 sum 10 sum 20 20");
+    parser.parse("repeat 3 [ repeat 2 [ fd 1 rt 2 ] rt 40 ]");
   }
 }
