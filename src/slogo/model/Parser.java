0aa8b566f0420ea8564f8296186e76e8b3e71e90
Livia <las120@duke.edu>
2021-03-07 13:56:11 -0500

    wrote comments to outline parser algorithm

--- src/slogo/model/Parser.java
+++ src/slogo/model/Parser.java
@@ -1,6 +1,7 @@
 package slogo.model;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ResourceBundle;
 import java.util.Stack;
 import slogo.controller.Controller;
 
@@ -9,6 +10,9 @@ public class Parser {
   private RegexDetector regexDetector = new RegexDetector();
   private CommandFactory commandFactory = new CommandFactory();
   private Controller controller;
+  private Stack<String> commandStack;
+  private Stack<String> argumentStack;
+  private static final String RESOURCES_PACKAGE = "resources.languages.";
 
   public Parser(Controller controller) {
     this.controller = controller;
@@ -21,10 +25,30 @@ public class Parser {
     String[] commandComponents = command.split(" ");
     for (String commandComponent: commandComponents) {
       String commandType = regexDetector.getSymbol(commandComponent);
+      if (commandType.equals("Constant")) {
+        commandStack.push(commandComponent);
+      } else {
+        commandStack.push(commandType);
+      }
       System.out.println(commandType);
       Object newCommand = commandFactory.createCommand(commandType);
     }
+    parseCommandStack();
     return 0;
+  }
+
+  private void parseCommandStack() {
+    ResourceBundle resources = ResourceBundle.getBundle(RESOURCES_PACKAGE + "English");
+    while (!commandStack.isEmpty()) {
+      if (!resources.containsKey(commandStack.peek())) {
+        argumentStack.push(commandStack.pop());
+      } else {
+        // pop command off command stack and determine how many arguments it takes
+        // if the argument stack is greater than or equal to the number of arguments needed, create
+        // a command object and pass it the two commands.
+        // execute this new command object and push it on to the argument stack.
+      }
+    }
   }
 
   public static void main(String[] args)
