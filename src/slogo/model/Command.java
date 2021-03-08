a46889ff4086605b1d0efc6fe743a38860c87a40
Livia <las120@duke.edu>
2021-03-08 13:44:03 -0500

    added more javadocs

--- src/slogo/model/Command.java
+++ src/slogo/model/Command.java
@@ -2,7 +2,24 @@ package slogo.model;
 
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * Command is the interface that all other Command classes implement.
+ *
+ * @author Livia Seibert and Rachel Luria
+ */
 public interface Command {
+
+  /**
+   * Calling execute on any command type will run the action of the command it's called on.
+   *
+   * @param turtle turtle object to execute command on
+   * @return double result of executing command
+   * @throws ClassNotFoundException
+   * @throws NoSuchMethodException
+   * @throws InvocationTargetException
+   * @throws InstantiationException
+   * @throws IllegalAccessException
+   */
   double execute(Turtle turtle)
       throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
 }
--- src/slogo/model/Constant.java
+++ src/slogo/model/Constant.java
@@ -1,13 +1,29 @@
 package slogo.model;
 
+/**
+ * Constant holds an integer value to be used as a parameter for
+ * a Command type.
+ *
+ * @author Rachel Luria and Livia Seibert
+ */
 public class Constant {
 
   private int value;
 
+  /**
+   * Constructor for Constant takes in an integer value.
+   *
+   * @param value integer value of the constant
+   */
   public Constant(int value) {
     this.value = value;
   }
 
+  /**
+   * Allows access to value of Constant.
+   *
+   * @return integer value of the constant
+   */
   public int getValue() {
     return value;
   }
