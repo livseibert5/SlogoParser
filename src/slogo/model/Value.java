4214993ab7a5d34b0650af1c1caf5e6cd7154cfb
Livia <las120@duke.edu>
2021-03-12 12:24:42 -0500

    added comments to Value class

--- src/slogo/model/Value.java
+++ src/slogo/model/Value.java
@@ -1,17 +1,39 @@
 package slogo.model;
 
-public class Value {
+/**
+ * Value class is the super type for Constant and Variable. Since both
+ * Constant and Variable can be passed to functions as arguments and both
+ * have a double value, they are both Value types.
+ *
+ * @author Livia Seibert
+ */
+public abstract class Value {
 
   private double value;
 
+  /**
+   * Value constructor assigns a double value to the object.
+   *
+   * @param value double value of object
+   */
   public Value(double value) {
     this.value = value;
   }
 
+  /**
+   * Allows access to assigned value.
+   *
+   * @return value of Value object
+   */
   public double getValue() {
     return value;
   }
 
+  /**
+   * Allows value to be set by parser.
+   *
+   * @param value double to set value variable to
+   */
   public void setValue(double value) {
     this.value = value;
   }
