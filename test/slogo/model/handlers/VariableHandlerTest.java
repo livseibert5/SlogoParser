f98c2681a38d29d6536324b598e0f3a618177f53
Livia <las120@duke.edu>
2021-03-12 15:41:42 -0500

    more handler tests

--- test/slogo/model/handlers/VariableHandlerTest.java
+++ test/slogo/model/handlers/VariableHandlerTest.java
@@ -1,5 +1,11 @@
 package slogo.model.handlers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import slogo.model.Variable;
+
 /**
  * Tests for VariableHandler class.
  *
@@ -7,4 +13,17 @@ package slogo.model.handlers;
  */
 public class VariableHandlerTest {
 
+  private VariableHandler variableHandler;
+
+  @BeforeEach
+  void setUp() {
+    variableHandler = new VariableHandler();
+  }
+
+  @Test
+  void addVariableWorks() {
+    Variable variable = new Variable(":var");
+    variableHandler.addVariable(variable);
+    assertEquals(variable, variableHandler.getVariableWithName(":var"));
+  }
 }
