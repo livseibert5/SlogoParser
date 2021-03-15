1003e0f44b449362294a53fbb5b35f674696cc40
Livia <las120@duke.edu>
2021-03-15 16:00:37 -0400

    added javadocs to display commands

--- src/slogo/model/displaycommand/PenColor.java
+++ src/slogo/model/displaycommand/PenColor.java
@@ -3,8 +3,20 @@ package slogo.model.displaycommand;
 import slogo.model.Command;
 import slogo.model.Turtle;
 
+/**
+ * PenColor is the Command subclass that returns the index of the current color
+ * of the turtle's pen.
+ *
+ * @author Livia Seibert
+ */
 public class PenColor implements Command {
 
+  /**
+   * Returns index of current color of turtle's pen.
+   *
+   * @param turtle turtle object to execute command on
+   * @return index of current color of turtle's pen
+   */
   @Override
   public double execute(Turtle turtle) {
     return turtle.getPenColor();
