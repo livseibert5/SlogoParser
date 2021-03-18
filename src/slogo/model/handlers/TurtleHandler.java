e656be8a7a2826144cd211a595c474f83b9b5b8a
Livia <las120@duke.edu>
2021-03-18 12:39:33 -0400

    got Ask to work and included AskTest

--- src/slogo/model/handlers/TurtleHandler.java
+++ src/slogo/model/handlers/TurtleHandler.java
@@ -58,7 +58,7 @@ public class TurtleHandler extends Observable {
   }
 
   public List<Turtle> getAllTurtles() {
-    return (List<Turtle>) turtles.values();
+    return new ArrayList<>(turtles.values());
   }
 
   public void setActiveTurtles(List<Turtle> newActiveTurtles) {
